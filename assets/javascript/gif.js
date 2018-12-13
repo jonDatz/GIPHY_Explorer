962767a1049279be0ad077769beddf6a588a4fe8
jonDatz <notjondatz@gmail.com>
2018-12-13 01:55:48 -0500

    added button logic, added ability to add buttons to screen, need to make div for GIFs then print them to screen. Run through it all and then figure out how to use localStorage to save t ousers computer

--- assets/javascript/gif.js
+++ assets/javascript/gif.js
@@ -1,19 +1,63 @@
 
-let buttons = [];
-let searchTerm = "Cat";
+let shows = ["Adventure Time", "Courage the Cowardly Dog", "Misadventures of Flapjack"];
 
 
 
 
 
+function displayShow (){
 
+let show = $(this).attr("data-name");
+let queryURL = "http://api.giphy.com/v1/gifs/search?q=" + show + "&api_key=wH45n8pCbWdmGgONQpIimBMLweZWUWKQ";
 
+console.log(queryURL);
+console.log(show);
+console.log(shows);
 $.ajax({
-    url: "http://api.giphy.com/v1/gifs/search?q=" + searchTerm + "&api_key=wH45n8pCbWdmGgONQpIimBMLweZWUWKQ",
+    url: queryURL,
     method: "GET"
   }).then(function(response) {
-      
+
     console.log(response);
 
 
   });
+
+
+}
+
+function showButtons () {
+    $('#buttonBox').empty();
+
+    for (let i = 0; i < shows.length; i++) {
+        var b = $("<button>");
+
+        b.addClass("btn btn-dark showButtons");
+
+        b.attr("data-name", shows[i]);
+
+        b.text("#" + shows[i]);
+
+        $("#buttonBox").append(b);
+        
+    }
+}
+
+
+$("#add-Show").on("click", function(event) {
+    event.preventDefault();
+    // This line of code will grab the input from the textbox
+    var show = $("#show-input").val().trim();
+
+    // The movie from the textbox is then added to our array
+    shows.push(show);
+
+    // Calling renderButtons which handles the processing of our movie array
+    showButtons();
+  });
+
+
+$(document).on("click", ".showButtons", displayShow);
+showButtons();
+
+//   Bootstrap button NEEDS: <button type="button" class="btn btn-dark">Dark</button>
